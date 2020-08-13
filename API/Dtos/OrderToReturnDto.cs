3c57b26fc41b0775c82c2a144921fc9a93adf3cb
Shalim Ahmed <roudro2010@gmail.com>
2020-08-13 23:58:57 +0600

    Section 18: API - Orders

---
+++ API/Dtos/OrderToReturnDto.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using Core.Entities.OrderAggregate;
+
+public class OrderToReturnDto
+    {
+        public int Id { get; set; }
+        public string BuyerEmail { get; set; }
+        public DateTimeOffset OrderDate { get; set; }
+        public Address ShipToAddress { get; set; }
+        public string DeliveryMethod { get; set; }
+        public decimal ShippingPrice { get; set; }
+        public IReadOnlyList<API.Dtos.OrderItemDto> OrderItems { get; set; }
+        public decimal Subtotal { get; set; }
+        public decimal Total { get; set; }
+        public string Status { get; set; }
+    }
